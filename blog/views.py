889a11b79a4aa93866bcf6cbb9539b9823372dd5
yangyang0126 <zhaojingyi0126@163.com>
2019-09-06 17:14:18 +0800

    update 2019.09.06

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,10 @@ def index(request):
 # 文章详情链接
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    
+    # 阅读量 +1
+    post.increase_views()
+    
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
