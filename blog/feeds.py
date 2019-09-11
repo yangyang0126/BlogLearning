652bfd2a45d756cc741a5c741de67124d735b5e6
yangyang0126 <zhaojingyi0126@163.com>
2019-09-11 14:45:42 +0800

    update 2019.09.11

---
+++ blog/feeds.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 13:09:40 2019
+
+@author: Administrator
+"""
+
+from django.contrib.syndication.views import Feed
+
+from .models import Post
+
+
+class AllPostsRssFeed(Feed):
+    # 显示在聚合阅读器上的标题
+    title = "洋阳的博客"
+
+    # 通过聚合阅读器跳转到网站的地址
+    link = "/"
+
+    # 显示在聚合阅读器上的描述信息
+    description = "To learn, To teach"
+
+    # 需要显示的内容条目
+    def items(self):
+        return Post.objects.all()
+
+    # 聚合器中显示的内容条目的标题
+    def item_title(self, item):
+        return '[%s] %s' % (item.category, item.title)
+
+    # 聚合器中显示的内容条目的描述
+    def item_description(self, item):
+        return item.body
--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,7 +3,9 @@
 设置模板标签
 """
 from django import template
-from ..models import Post, Category
+from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
+
 
 register = template.Library()
 
@@ -19,5 +21,11 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    """分类"""
-    return Category.objects.all()
+    # 记得在顶部引入 count 函数
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag
+def get_tags():
+    # 记得在顶部引入 Tag model
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
