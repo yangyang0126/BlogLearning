bafa57a5329680f8c5812ee31b398148c1e0dee0
unknown <zhaojingyi0126@163.com>
2019-09-15 15:01:08 +0800

    update 2019.09.15

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@
 from django import template
 from ..models import Post, Category, Tag
 from django.db.models.aggregates import Count
-
+import os
 
 register = template.Library()
 
@@ -29,3 +29,29 @@ def get_categories():
 def get_tags():
     # 记得在顶部引入 Tag model
     return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+    
+@register.simple_tag
+def get_foodpicture_1():
+  
+    abspath = os.getcwd()
+    filePath = abspath + "\\blog\\static\\images_food\\1\\"
+    title = []
+    path = []
+    for EveryPath in os.listdir(filePath):
+        title.append(EveryPath[:-4])
+        path.append("static\\images_food\\1\\"+EveryPath)
+    return zip(title,path)
+    
+@register.simple_tag
+def get_foodpicture_2():
+  
+    abspath = os.getcwd()
+    filePath = abspath + "\\blog\\static\\images_food\\2\\"
+    title = []
+    path = []
+    for EveryPath in os.listdir(filePath):
+        title.append(EveryPath[:-4])
+        path.append("static\\images_food\\2\\"+EveryPath)
+    return zip(title,path)
+    
+    
